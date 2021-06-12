23976439d40e694ab80755fa7d939944fe0f03aa
jroshanucb <ubuntu@ip-172-31-39-101.us-west-2.compute.internal>
2021-06-12 20:22:44 +0000

    update lr frequency

--- imagenet.py
+++ imagenet.py
@@ -488,7 +488,7 @@ class ProgressMeter(object):
 
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = LR * (0.1 ** (epoch // 20))
+    lr = LR * (0.1 ** (epoch // 10))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
